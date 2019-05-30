e766670226a3ab17f1c59454635fb785f1e8e031
EoVlIdL <39077502+EoVlIdL@users.noreply.github.com>
2019-05-31 06:36:32 +0800

    new_one

--- transfer/bcon2.cpp
+++ transfer/bcon2.cpp
@@ -8,5 +8,29 @@ using namespace Eigen;
 
 int main(){
 	string s ="-0.12026328 -0.02369919 0.0794071 -0.07300829 0.02144894 0.1731819 0.14079559 0.08050600 0.13548067 -0.04575408 0.04896416 -0.02175735 0.17480594 0.07393119 0.03627094 -0.0374101";
-	
+	string::size_type pos1, pos2;
+	pos2 = s.find(" ");
+	string b = " ";
+	vector<string> v;
+	vector<double> v1;
+	pos1 = 0;
+	while(string::npos != pos2){
+		//cout<<"enterloop"<<endl;
+		v.push_back(s.substr(pos1, pos2-pos1));
+         
+       		pos1 = pos2 + b.size();
+        	pos2 = s.find(b, pos1);
+	}
+	v.push_back(s.substr(pos1,10));
+	for(int i = 0; i<16; i++){
+		cout<<v[i]<<endl;		
+	}
+	//cout<<"outloop"<<endl;
+	for(int i = 0; i<16; i++){
+		v1.push_back(stod(v[i]));		
+	}
+	//cout<<"outstod"<<endl;
+	//for(int i = 0; i<16; i++){
+	//	cout<<v1[i]<<endl;		
+	//}
 }
